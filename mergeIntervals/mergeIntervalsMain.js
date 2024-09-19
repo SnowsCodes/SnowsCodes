4be46a7fa51637af226fae2858e33af6422cc66c
Alice Fan <137368570+SnowsCodes@users.noreply.github.com>
2024-09-19 11:18:16 -0700

    Update mergeIntervalsMain.js

--- mergeIntervals/mergeIntervalsMain.js
+++ mergeIntervals/mergeIntervalsMain.js
@@ -1,5 +1,5 @@
 //the intervals you want to merge :)
-var intervals = [[1, 2], [3, 4], [2, 4]]; 
+var intervals = [[1, 2], [3, 4], [1, 4]]; 
 var length = intervals.length; 
 
 function merge (a1, a2) {
@@ -16,14 +16,59 @@ function merge (a1, a2) {
     }
 }
 
-if (length == 2) {
-    var result = merge(intervals[0], intervals[1]); 
-} else if (length > 2) {
-    for (var i = 0; i < length!; i++) {
-        
+function repeat (array) {
+    var num = array.length;
+    var rep = 1; 
+    for (var i = 1; i < num; i++) {
+        rep = rep * i; 
     }
+    var rep = rep/2; 
+    var a = 0; 
+    var b = 1; 
+    var index = [[a, b]]; 
+    for (var i = 0; i < rep + 1; i++) {
+        if (b != array.length-1) {
+            b++; 
+        } else {
+            a++; 
+            b = a+1; 
+        }
+        index.push([a, b]); 
+    }
+    
+    for (var i = 0; i < index.length; i++) {
+        console.log(i); 
+        var temp = merge(intervals[index[i][0]], intervals[index[i][1]]); 
+        console.log (intervals[index[i][0]] + "   " + intervals[index[i][1]]);
+        console.log("temp length is: ");
+        console.log(temp.length); 
+        if (temp.length == 2) {
+            console.log("ran this"); 
+            array.splice(index[i][1], 1); 
+            array.splice(index[i][0], 1); 
+            array.push(temp); 
+            return [array, true]; 
+        }
+    }
+
+    return [array, false]; 
+    
+}
+
+var result; 
+
+if (length == 2) {
+    result = merge(intervals[0], intervals[1]); 
+} else if (length > 2) {
+    var tempResult = repeat(intervals); 
+    console.log(tempResult); 
+    while (tempResult[1] && tempResult[0].length > 1) {
+        var tempResult = repeat(intervals); 
+        console.log(tempResult);
+    }
+    result = tempResult[0]; 
 } else {
     result = intervals; 
 }
 
-console.log(result); 
+console.log(result[0]); 
