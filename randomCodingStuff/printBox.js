b0ba09201dadc013cabb3e72ac6bee9b14d6a192
Alice Fan <137368570+SnowsCodes@users.noreply.github.com>
2024-10-25 12:27:42 -0700

    IT WORKSSS I think

--- randomCodingStuff/printBox.js
+++ randomCodingStuff/printBox.js
@@ -35,7 +35,6 @@ function isntObject (name, input) {
         
         if (i < name.length-1) {
             rows[0] += "┬" + name[i+1];
-            //rows[1] += "│ ";
             for (var j = 1; j < rows.length-1; j++) {
                 rows[j] += "│ ";
             }
@@ -44,16 +43,20 @@ function isntObject (name, input) {
             if (typeof input[name[i+1]] != typeof {}) {
                 rows[1] += input[name[i+1]] + " ";
             } else {
-                
                 var len = rows.length; 
                 for (var j = rows.length; j < input[name[i+1]].length; j++) {
                     rows.push(empty); 
+                    if (j != len) {
+                      rows.push(empty); 
+                      j++; 
+                    }
                 }
                 
                 if (rows.length > len) {
                     rows.push(rows[len-1]); 
                     rows[len-1] = empty; 
                 }
+                
                 for (var j = 0; j < input[name[i+1]].length; j++) {
                   rows[j+1] += input[name[i+1]][j];
                 }
@@ -72,27 +75,25 @@ function isntObject (name, input) {
         returnVal += rows[i] + "\n"
     }
     return returnVal; 
-    
-    //return (rows[0] + "\n" + rows[1] + "\n" + rows[rows.length-1]);
-    //return [rows[0], rows[1], rows[rows.length-1]];
 }
 
 function printBox (name, input) {
     if (typeof input == typeof {}) {
         var index = Object.getOwnPropertyNames(input); 
         var len = index.length;
+        var otherLen = index.length; 
         var objects = {}; 
         var indexOfObj = []; 
         for (var i = index.length-1; i >= 0; i--) {
             if (typeof input[index[i]] == typeof {}) {
                 objects[i] = printBox(index[i], input[index[i]]).split("\n");
                 input[index[i]] = objects[i];
-                index.length -= 1;    
+                otherLen -= 1;    
             }
         }
         
         
-        if (index.length == len) {
+        if (otherLen == len) {
             var out = isntObject(index, input);
         } else {
             var temp = isntObject(index, input).split("\n");
@@ -131,3 +132,6 @@ inputArray = [1, [1, 2], [1, 2], 2, 3];
 console.log("out is: \n" + printBox("aa", inputArray));
 inputArray = [1, [1, 2], [1, ["a", "b"]], 2, 3];
 console.log("out is: \n" + printBox("aa", inputArray));
+
+
+
